04c6d4d049c1c4f3a8460f97c8bbc8bc822994ca
SergeyMell <sergey.mell@agilie.com>
2019-02-15 15:09:57 +0200

    Implemented additional controllers for lists

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     namespace :marketing do
       resources :lists, only: %i[show index]
+
+      namespace :list do
+        resources :abandoned_carts, only: %i[show]
+        resources :favourable_products, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :least_active_users, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :least_zone_wise_orders, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :most_active_users, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :most_discounted_orders, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :most_searched_keywords, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :most_used_payment_methods, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :most_zone_wise_orders, only: %i[show], controller: '/spree/admin/marketing/lists'
+        resources :new_users, only: %i[show], controller: '/spree/admin/marketing/lists'
+      end
+
+
       resources :campaigns, only: %i[show index] do
         member do
           get :display_recipient_emails
